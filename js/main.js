f7a9874807384f54bb77e7c797db8d73fb370965
Rapid1898 <markuspolzer@gmx.at>
2021-05-18 23:13:48 +0200

    upd

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(function() {
     $(document).ready(function() {
       $('#example').DataTable();
+      // $('#example').DataTable( {
+      //   paging: false,
+      //   scrollY: 400
+      // } );
+      // $('#example').DataTable( {
+      //   select: true
+      // } );
     });
   });
