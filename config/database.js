0b43676b20d0ea38b12f9ed83373e3c018abab11
Rapid1898 <markuspolzer@gmx.at>
2021-05-19 23:13:29 +0200

    upd

--- config/database.js
+++ config/database.js
@@ -1,24 +1,33 @@
 const Sequelize = require('sequelize')
-
-// const sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPW, {
-//     host: process.env.DBHOST,
-//     dialect: 'mariadb',
-//     dialectOptions: {connectTimeout: 1000}
-//   });
+const Scores = require('../models/Scores')
 
 const connectDB = async () => {
     try {
-        const sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPW, {
+        const sequelize = new Sequelize(process.env.DBN, process.env.DBUSER, process.env.DBPW, {
             host: process.env.DBHOST,
             dialect: 'mariadb',
-            dialectOptions: {connectTimeout: 1000}
+            dialectOptions: {connectTimeout: 5000}
         });
-        console.log(sequelize.config)
-        // console.log(`DB Connected: ${host}`)
+        console.log(`DB Connected: ${sequelize.config.host} ${sequelize.config.host}...`)
+
+        try {
+          await sequelize.authenticate();
+          console.log('Connection has been established successfully.');
+        } 
+          catch (error) {
+          console.error('Unable to connect to the database:', error);
+        } 
+
     } catch (err) {
-      console.error(err)
+      console.error('Unable to connect to the database:', error);
       process.exit(1)
     }
   }
-  
-  module.exports = connectDB
+
+// Find all users
+const scores = Scores.findAll();
+console.log(scores.every(scores => scores instanceof User)); // true
+console.log("All users:", JSON.stringify(scores, null, 2));
+
+
+module.exports = connectDB
