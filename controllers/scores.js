035f6d40a9efbf088f423d1452fdc0fa8fc697da
Rapid1898 <markuspolzer@gmx.at>
2021-05-20 22:44:21 +0200

    upd

--- controllers/scores.js
+++ controllers/scores.js
@@ -9,7 +9,9 @@ module.exports = {
                 (err,rows) => {
                     if(err) throw err;  
                     rows.forEach( (row) => {
-                        console.log(`${row.ticker} ${row.indexName} ${row.scoreFull}`);
+                        // console.log(`${row.ticker} ${row.calcDate} ${row.indexName} ${row.scoreFull}`);
+                        // console.log(`${typeof row.ticker} ${typeof row.calcDate}`)
+                        console.log(`${row.calcDate.toISOString().split('T')[0]}`)
                     });
                     res.render('scores.ejs', {rows: rows})                            
                 });
