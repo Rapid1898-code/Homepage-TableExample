8abf3c06988096086f9be5a80704842e6669d214
Rapid1898 <rapid1898@gmail.com>
2021-05-22 23:14:27 +0200

    upd

--- config/database.js
+++ config/database.js
@@ -2,15 +2,15 @@ const mysql = require('mysql');
 require('dotenv').config({path: './config/.env'})
 
 const connectDB = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: process.env.DBPW_LOCAL,
-  database: 'levermann'  
+  // host: 'localhost',
+  // user: 'root',
+  // password: process.env.DBPW_LOCAL,
+  // database: 'levermann'  
   
-  // host: 'nl1-ss18.a2hosting.com',
-  // user: 'rapidtec_Rapid1898',  
-  // password: process.env.DBPW_A2HOSTED,  
-  // database: 'rapidtec_levermann'  
+  host: 'nl1-ss18.a2hosting.com',
+  user: 'rapidtec_Rapid1898',  
+  password: process.env.DBPW_A2HOSTED,  
+  database: 'rapidtec_levermann'  
 });
 connectDB.connect((err) => {
     if(err){
