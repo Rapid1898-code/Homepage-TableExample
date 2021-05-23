f1b5bac13792492ab69f56bf1a2de74c9dcca246
Rapid1898 <markuspolzer@gmx.at>
2021-05-23 15:46:02 +0200

    upd

--- public/js/main.js
+++ public/js/main.js
@@ -6,8 +6,8 @@ $(function() {
         autoWidth: false, //step 1
         columnDefs: [
           { width: '900px', targets: 0 }, //step 2, column 1 out of 4
-          { width: '500px', targets: 1 }, //step 2, column 2 out of 4
-          { width: '300px', targets: 2 }  //step 2, column 3 out of 4
+          { width: '900px', targets: 1 }, //step 2, column 2 out of 4
+          { width: '100px', targets: 2 }  //step 2, column 3 out of 4
         ]
       } );
 
